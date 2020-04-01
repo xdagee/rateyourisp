e41966be6b2da5385bca8a92363f93fdeaaed075
princegee <14103448+xdagee@users.noreply.github.com>
2020-04-01 12:47:42 +0000

    fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,187 +4,225 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
 
-	var	$window = $(window),
+	var $window = $(window),
 		$body = $('body'),
 		$sidebar = $('#sidebar');
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ null,      '480px'  ]
-		});
+	breakpoints({
+		xlarge: ['1281px', '1680px'],
+		large: ['981px', '1280px'],
+		medium: ['737px', '980px'],
+		small: ['481px', '736px'],
+		xsmall: [null, '480px']
+	});
 
 	// Hack: Enable IE flexbox workarounds.
-		if (browser.name == 'ie')
-			$body.addClass('is-ie');
+	if (browser.name == 'ie')
+		$body.addClass('is-ie');
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	$window.on('load', function () {
+		window.setTimeout(function () {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
 	// Forms.
 
-		// Hack: Activate non-input submits.
-			$('form').on('click', '.submit', function(event) {
+	// Hack: Activate non-input submits.
+	$('form').on('click', '.submit', function (event) {
 
-				// Stop propagation, default.
-					event.stopPropagation();
-					event.preventDefault();
+		// Stop propagation, default.
+		event.stopPropagation();
+		event.preventDefault();
 
-				// Submit form.
-					$(this).parents('form').submit();
+		// Submit form.
+		$(this).parents('form').submit();
+
+	});
+
+	// Sidebar.
+	if ($sidebar.length > 0) {
+
+		var $sidebar_a = $sidebar.find('a');
+
+		$sidebar_a
+			.addClass('scrolly')
+			.on('click', function () {
+
+				var $this = $(this);
+
+				// External link? Bail.
+				if ($this.attr('href').charAt(0) != '#')
+					return;
+
+				// Deactivate all links.
+				$sidebar_a.removeClass('active');
+
+				// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+				$this
+					.addClass('active')
+					.addClass('active-locked');
+
+			})
+			.each(function () {
+
+				var $this = $(this),
+					id = $this.attr('href'),
+					$section = $(id);
+
+				// No section for this link? Bail.
+				if ($section.length < 1)
+					return;
+
+				// Scrollex.
+				$section.scrollex({
+					mode: 'middle',
+					top: '-20vh',
+					bottom: '-20vh',
+					initialize: function () {
+
+						// Deactivate section.
+						$section.addClass('inactive');
+
+					},
+					enter: function () {
+
+						// Activate section.
+						$section.removeClass('inactive');
+
+						// No locked links? Deactivate all links and activate this section's one.
+						if ($sidebar_a.filter('.active-locked').length == 0) {
+
+							$sidebar_a.removeClass('active');
+							$this.addClass('active');
+
+						}
+
+						// Otherwise, if this section's link is the one that's locked, unlock it.
+						else if ($this.hasClass('active-locked'))
+							$this.removeClass('active-locked');
+
+					}
+				});
 
 			});
 
-	// Sidebar.
-		if ($sidebar.length > 0) {
-
-			var $sidebar_a = $sidebar.find('a');
-
-			$sidebar_a
-				.addClass('scrolly')
-				.on('click', function() {
-
-					var $this = $(this);
-
-					// External link? Bail.
-						if ($this.attr('href').charAt(0) != '#')
-							return;
-
-					// Deactivate all links.
-						$sidebar_a.removeClass('active');
-
-					// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-						$this
-							.addClass('active')
-							.addClass('active-locked');
-
-				})
-				.each(function() {
-
-					var	$this = $(this),
-						id = $this.attr('href'),
-						$section = $(id);
-
-					// No section for this link? Bail.
-						if ($section.length < 1)
-							return;
-
-					// Scrollex.
-						$section.scrollex({
-							mode: 'middle',
-							top: '-20vh',
-							bottom: '-20vh',
-							initialize: function() {
-
-								// Deactivate section.
-									$section.addClass('inactive');
-
-							},
-							enter: function() {
-
-								// Activate section.
-									$section.removeClass('inactive');
-
-								// No locked links? Deactivate all links and activate this section's one.
-									if ($sidebar_a.filter('.active-locked').length == 0) {
-
-										$sidebar_a.removeClass('active');
-										$this.addClass('active');
-
-									}
-
-								// Otherwise, if this section's link is the one that's locked, unlock it.
-									else if ($this.hasClass('active-locked'))
-										$this.removeClass('active-locked');
-
-							}
-						});
-
-				});
-
-		}
+	}
 
 	// Scrolly.
-		$('.scrolly').scrolly({
-			speed: 1000,
-			offset: function() {
+	$('.scrolly').scrolly({
+		speed: 1000,
+		offset: function () {
 
-				// If <=large, >small, and sidebar is present, use its height as the offset.
-					if (breakpoints.active('<=large')
-					&&	!breakpoints.active('<=small')
-					&&	$sidebar.length > 0)
-						return $sidebar.height();
+			// If <=large, >small, and sidebar is present, use its height as the offset.
+			if (breakpoints.active('<=large')
+				&& !breakpoints.active('<=small')
+				&& $sidebar.length > 0)
+				return $sidebar.height();
 
-				return 0;
+			return 0;
+
+		}
+	});
+
+	// Spotlights.
+	$('.spotlights > section')
+		.scrollex({
+			mode: 'middle',
+			top: '-10vh',
+			bottom: '-10vh',
+			initialize: function () {
+
+				// Deactivate section.
+				$(this).addClass('inactive');
+
+			},
+			enter: function () {
+
+				// Activate section.
+				$(this).removeClass('inactive');
+
+			}
+		})
+		.each(function () {
+
+			var $this = $(this),
+				$image = $this.find('.image'),
+				$img = $image.find('img'),
+				x;
+
+			// Assign image.
+			$image.css('background-image', 'url(' + $img.attr('src') + ')');
+
+			// Set background position.
+			if (x = $img.data('position'))
+				$image.css('background-position', x);
+
+			// Hide <img>.
+			$img.hide();
+
+		});
+
+	// Features.
+	$('.features')
+		.scrollex({
+			mode: 'middle',
+			top: '-20vh',
+			bottom: '-20vh',
+			initialize: function () {
+
+				// Deactivate section.
+				$(this).addClass('inactive');
+
+			},
+			enter: function () {
+
+				// Activate section.
+				$(this).removeClass('inactive');
 
 			}
 		});
 
-	// Spotlights.
-		$('.spotlights > section')
-			.scrollex({
-				mode: 'middle',
-				top: '-10vh',
-				bottom: '-10vh',
-				initialize: function() {
 
-					// Deactivate section.
-						$(this).addClass('inactive');
 
-				},
-				enter: function() {
+	/* AjaxChimp
+     * ------------------------------------------------------ */
+    var ssAjaxChimp = function() {
 
-					// Activate section.
-						$(this).removeClass('inactive');
+        $('#mc-form').ajaxChimp({
+            language: 'es',
+            url: 'https://saveourdata.us4.list-manage.com/subscribe/post?u=fcd9a848b5ff4493603c71337&amp;id=3fa4d197d4'
+        });
 
-				}
-			})
-			.each(function() {
+        // Mailchimp translation
+        //
+        //  Defaults:
+        //   'submit': 'Submitting...',
+        //  0: 'We have sent you a confirmation email',
+        //  1: 'Please enter a value',
+        //  2: 'An email address must contain a single @',
+        //  3: 'The domain portion of the email address is invalid (the portion after the @: )',
+        //  4: 'The username portion of the email address is invalid (the portion before the @: )',
+        //  5: 'This email address looks fake or invalid. Please enter a real email address'
 
-				var	$this = $(this),
-					$image = $this.find('.image'),
-					$img = $image.find('img'),
-					x;
+        $.ajaxChimp.translations.es = {
+            'submit': 'Submitting...',
+            0: '<i class="fas fa-check"></i> Congrats! We have sent you a confirmation email',
+            1: '<i class="fas fa-exclamation-triangle"></i> You must enter a valid e-mail address.',
+            2: '<i class="fas fa-exclamation-triangle"></i> E-mail address is not valid.',
+            3: '<i class="fas fa-exclamation-triangle"></i> E-mail address is not valid.',
+            4: '<i class="fas fa-exclamation-triangle"></i> E-mail address is not valid.',
+            5: '<i class="fas fa-exclamation-triangle"></i> E-mail address is not valid.'
+        }
+    };
 
-				// Assign image.
-					$image.css('background-image', 'url(' + $img.attr('src') + ')');
 
-				// Set background position.
-					if (x = $img.data('position'))
-						$image.css('background-position', x);
-
-				// Hide <img>.
-					$img.hide();
-
-			});
-
-	// Features.
-		$('.features')
-			.scrollex({
-				mode: 'middle',
-				top: '-20vh',
-				bottom: '-20vh',
-				initialize: function() {
-
-					// Deactivate section.
-						$(this).addClass('inactive');
-
-				},
-				enter: function() {
-
-					// Activate section.
-						$(this).removeClass('inactive');
-
-				}
-			});
+	(function ssInit() {
+		ssAjaxChimp();
+	})();
 
 })(jQuery);
